ea2f8490073f8f54f02bbe9d96be19fd6bbe1543
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2022-10-31 07:08:23 +0300

    completed code-challenge

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,33 @@
 require_relative 'article'
+
 class Author
   attr_reader :name, :articles, :magazines
 
+
   def initialize(name)
-    @name = name
+    @name = name   
   end
+ 
+  def articles
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines
+    articles.map do |article|
+      article.magazine
+    end.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(magazine, title)
+  end
+
+  def topic_areas
+    magazines.map do |magaz|
+      magaz.category
+    end
+  end
+end
+
